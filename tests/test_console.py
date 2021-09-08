24d0755c057820cdf5f80c0fef72ec6cd0f9f726
makar-pelogeiko <m.pelogeiko@mail.ru>
2021-09-08 22:27:26 +0300

    -correct info in test added

--- tests/test_console.py
+++ tests/test_console.py
@@ -12,5 +12,5 @@ def test_save_to_dot():
     dot_graph = pydot.graph_from_dot_file("tests/data/graph1.dot")[0]
     nx_graph = networkx.drawing.nx_pydot.from_pydot(dot_graph)
     assert nx_graph.number_of_nodes() == 6
-    assert nx_graph.number_of_edges() == 6
+    assert nx_graph.number_of_edges() == 7
     assert cfpq_data.get_labels(graph, verbose=False) == {"a", "b"}
