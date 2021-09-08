1e109fcff76c0e9de5453e808725b920dfd75961
makar-pelogeiko <m.pelogeiko@mail.ru>
2021-09-08 22:07:43 +0300

    trying to test with test

---
+++ project/console_app.py
@@ -0,0 +1,53 @@
+import cfpq_data
+import console_lib
+import networkx
+
+
+def input_int(text):
+    print(text)
+    while (True):
+        num = input()
+        if num.isdigit():
+            return int(num)
+        print('need integer positive number (or zero)')
+
+
+def call_get_info():
+    name = input('input name of graph: ')
+    console_lib.get_graph_info(name)
+
+
+def call_generate_and_save_graph():
+    first = input_int('number of nodes in first cycle')
+    second = input_int('number of nodes in second cycle')
+    left = input('input left label: ')
+    right = input('input right label: ')
+    label = [left, right]
+    graph = console_lib.generate_two_cycle_graph(first, second, label)
+    path = input('type the path to save .DOT file')
+    console_lib.save_graph_dot(graph, path)
+
+
+def menu(items_lst: list[str]):
+    while (1 == 1):
+        print(f"""{items_lst[0]}\n""")
+        num = 1
+        for item in items_lst[1:]:
+            print(f"""{num} - {item}""")
+            num = num + 1
+        response = input('input number: ')
+        if (response.isdigit()):
+            int_resp = int(response)
+            if (int_resp > 0) and (int_resp < num):
+                return int_resp
+        print('you did not chose corect. type number of menu-element and hit <enter>')
+
+
+def run_console_app():
+    func_items = [call_get_info, call_generate_and_save_graph]
+    while True:
+        menu_lst = ['menu list:', 'exit', 'get info about graph', 'create 2 cycles graph and save it to .DOT']
+        element = menu(items_lst=menu_lst) - 2
+        if element == -1:
+            return
+        func_items[element]()
---
+++ project/console_lib.py
@@ -0,0 +1,33 @@
+import cfpq_data
+import networkx
+from typing import Tuple
+from pathlib import Path
+from networkx import MultiDiGraph
+import pydot
+
+
+def get_graph_info(name: str):
+    graph = cfpq_data.graph_from_dataset(name, verbose=False)
+    print(f"""info about {name}:\n""")
+    print(
+        f"""Nodes: {graph.number_of_nodes()}\nEdges: {graph.number_of_edges()}\nLabels: {cfpq_data.get_labels(graph, verbose=False)}""")
+    print('end of graph info\n')
+
+
+"""
+По количеству вершин в циклах и именам меток
+строить граф из двух циклов и сохранять его в указанный файл в формате DOT (использовать pydot).
+"""
+
+
+def generate_two_cycle_graph(nodes_first: int, nodes_second: int, labels: Tuple[str, str]):
+    return cfpq_data.labeled_two_cycles_graph(nodes_first, nodes_second, edge_labels=labels, verbose=False)
+
+
+def save_graph_dot(graph: MultiDiGraph, full_path: str):
+    graph_dot = networkx.drawing.nx_pydot.to_pydot(graph)
+    file = open(full_path, "w")
+    graph_dot.write_raw(full_path)
+    file.close()
+    print(f"Graph was saved in {full_path}")
+    return full_path
