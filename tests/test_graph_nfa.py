ed97c1bb9f21f8be8bc4f2c290954ab3f0585c88
makar-pelogeiko <m.pelogeiko@mail.ru>
2021-09-22 13:46:44 +0300

    [Task 3] - requirements.txt codestyle - test_graph_nfa.py @pytest.fixture used

--- tests/test_graph_nfa.py
+++ tests/test_graph_nfa.py
@@ -1,32 +1,38 @@
 from project.graph_nfa import nfa_from_graph
 from pyformlang.finite_automaton import Symbol
 from project import graph_utils
+import pytest
 
-graph = graph_utils.generate_two_cycle_graph(2, 3, ("a", "b"))
+
+@pytest.fixture
+def graph():
+    return graph_utils.generate_two_cycle_graph(2, 3, ("a", "b"))
 
 
-def test_non_deterministic():
-    nfa = nfa_from_graph(graph)
+@pytest.fixture
+def nfa():
+    return nfa_from_graph(graph_utils.generate_two_cycle_graph(2, 3, ("a", "b")))
+
+
+def test_non_deterministic(nfa):
     assert not nfa.is_deterministic()
 
 
-def test_word_accept():
+def test_word_accept(nfa):
     symbol_a = Symbol("a")
     symbol_b = Symbol("b")
-    nfa = nfa_from_graph(graph)
     # starts from state 1 ends in state 3 (nfa goes 1 ->(a) 2 ->(a) 0 ->(b) 3) - all states can be finale and start
     assert nfa.accepts([symbol_a, symbol_a, symbol_b])
 
 
-def test_word_not_accept():
+def test_word_not_accept(nfa):
     symbol_a = Symbol("a")
     symbol_b = Symbol("b")
-    nfa = nfa_from_graph(graph)
     # all states can be finale and start, but there is no way in graph with such labels
     assert not nfa.accepts([symbol_a, symbol_b, symbol_a])
 
 
-def test_word_special_state_accept():
+def test_word_special_state_accept(graph):
     symbol_a = Symbol("a")
     nfa = nfa_from_graph(graph, start_nodes=set([1]), finale_nodes=set([0]))
     # start state 1 and finale state 0, nfa goes 1 ->(a) 2 ->(a) 0
