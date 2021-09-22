c97377cab707f4457d29ee9a44821792db502a9d
makar-pelogeiko <m.pelogeiko@mail.ru>
2021-09-22 17:47:38 +0300

    __init__.py improved

--- project/__init__.py
+++ project/__init__.py
@@ -1,1 +1,11 @@
-print("import sources directory")
+import project.console
+from project.console import *
+
+import project.graph_utils
+from project.graph_utils import *
+
+import project.regexp_dfa
+from project.regexp_dfa import *
+
+import project.graph_nfa
+from project.graph_nfa import *
