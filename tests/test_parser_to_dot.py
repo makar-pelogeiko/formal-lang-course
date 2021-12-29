eb8952fe6308c1c1f10156821088afdef2b58916
makar-pelogeiko <m.pelogeiko@mail.ru>
2021-12-29 18:39:04 +0300

    [Task 15] - missing slash added

--- tests/test_parser_to_dot.py
+++ tests/test_parser_to_dot.py
@@ -1,7 +1,7 @@
 import pytest
 from project.parser.parser_invoker import write_to_dot
 
-path = "tests\data\\parsed_grammar.dot"
+path = "tests\\data\\parsed_grammar.dot"
 
 
 def test_write_to_dot():
@@ -11,7 +11,7 @@ let Iquery1 = ('type' || Il1)**\n"""
     status = write_to_dot(line, path)
     obtained = open(path, "r")
 
-    path_expected = "tests\data\\parsed_grammar_expected.dot"
+    path_expected = "tests\\data\\parsed_grammar_expected.dot"
     expected = open(path_expected, "r")
     assert (expected.read() == obtained.read()) and status
 
